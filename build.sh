7b68744ff31c52193ca291f6b7036ea53cc5da2a
DongYongchang <dongyongchang@roboscience.ai>
2025-09-17 15:24:21 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+if [ "${1}" == "clear" ];then
+  echo "Clear ...... "
+  rm -rf build
+  rm -rf install
+elif [ "${1}" == "init" ];then
+  echo "Init ...... "
+  git submodule init
+  git submodule update
+elif [ "${1}" == "uinit" ];then
+  echo "Delete ...... "
+  rm -rf build
+  rm -rf install
+else
+  echo "Building ..."
+  mkdir build
+  cd build
+
+  # exit on failure
+  set -e
+
+  cmake ../ \
+    -DCMAKE_INSTALL_PREFIX=./install \
+    -DCMAKE_BUILD_TYPE=Release
+
+  ##################################################################################
+  # Exprot MBuild tool path on windwos platform
+  ##################################################################################
+  OS_TYPE=$(uname -s)
+
+  case "$OS_TYPE" in
+      Linux*)
+          echo "Linux Platform"
+          make -j4
+          make install
+
+          ;;
+      Darwin*)
+          echo "macOS Platform"
+          # macOS系统上的命令或操作
+          ;;
+      CYGWIN*|MINGW32*|MSYS*|MINGW*)
+          echo "Windows Platform"
+
+          cmake --build . --config Release --target install
+
+          if [ -f $thirdparty_manage_mbuild_tool_path ]; then
+            echo "thirdparty_manage_mbuild_tool_path no set"
+            exit -1
+          fi
+
+          "$thirdparty_manage_mbuild_tool_path" *.sln -maxcpucount:3
+          #"$thirdparty_manage_mbuild_tool_path"  INSTALL.vcxproj
+          
+          ;;
+      *)
+          echo "Unknow Platform: $OS_TYPE"
+          # 其他未知系统的命令或操作
+          ;;
+  esac
+
+  cd ..
+fi
+
+
+echo "Done"
